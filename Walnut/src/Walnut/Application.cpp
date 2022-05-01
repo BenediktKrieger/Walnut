49e82d5800aafa7e2ecf391dd78222326dedd2a8
BenediktKrieger <benediktkrieger@uni-koblenz.de>
2022-05-01 21:16:57 +0200

    default font size 18px

--- Walnut/src/Walnut/Application.cpp
+++ Walnut/src/Walnut/Application.cpp
@@ -473,7 +473,7 @@ namespace Walnut {
 		// Load default font
 		ImFontConfig fontConfig;
 		fontConfig.FontDataOwnedByAtlas = false;
-		ImFont* robotoFont = io.Fonts->AddFontFromMemoryTTF((void*)g_RobotoRegular, sizeof(g_RobotoRegular), 20.0f, &fontConfig);
+		ImFont* robotoFont = io.Fonts->AddFontFromMemoryTTF((void*)g_RobotoRegular, sizeof(g_RobotoRegular), 18.0f, &fontConfig);
 		io.FontDefault = robotoFont;
 
 		// Upload Fonts
